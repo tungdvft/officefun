37ff0d8e2c439eac962cb499deb6a0a9f8eee16e
tungdvft <tung.dvft@gmail.com>
2025-03-31 15:43:04 +0700

    Create nick-name.vue

---
+++ pages/dashboard/nick-name.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mx-auto">
+    <!-- Các phần khác của index.vue -->
+    <NicknameGenerator />
+  </div>
+</template>
+
+<script setup>
+import NicknameGenerator from '~/components/NicknameGenerator.vue';
+definePageMeta({
+  layout: 'dashboard'
+});
+</script>
