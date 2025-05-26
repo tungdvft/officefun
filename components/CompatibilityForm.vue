5f555828c1032ecb51ca3dbc57418ba2514afe3c
AIQRART <support@aiqrart.co>
2025-05-26 17:14:22 +0700

    update

--- components/CompatibilityForm.vue
+++ components/CompatibilityForm.vue
@@ -46,11 +46,6 @@
                     @focus="clearError"
                     :max="maxDate"
                   />
-                  <span class="absolute right-3 top-3 text-gray-400">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                      <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
-                    </svg>
-                  </span>
                 </div>
                 <p v-if="dob1 && lifePath1" class="text-sm mt-2 text-purple-600">
                   Số đường đời: {{ lifePath1 }} {{ isMasterNumber(lifePath1) ? '(Số đặc biệt)' : '' }}
@@ -92,11 +87,7 @@
                     @focus="clearError"
                     :max="maxDate"
                   />
-                  <span class="absolute right-3 top-3 text-gray-400">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                      <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
-                    </svg>
-                  </span>
+                 
                 </div>
                 <p v-if="dob2 && lifePath2" class="text-sm mt-2 text-purple-600">
                   Số đường đời: {{ lifePath2 }} {{ isMasterNumber(lifePath2) ? '(Số đặc biệt)' : '' }}
